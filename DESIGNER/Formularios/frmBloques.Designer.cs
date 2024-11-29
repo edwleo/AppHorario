5b724b830cfc64c46ebbf01a213e90f351e27973
Jhon Francia Minaya <jfranciaminaya@gmail.com>
2024-11-29 18:02:42 -0500

    Interfaz para registro de bloques - F1

---
+++ DESIGNER/Formularios/frmBloques.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace DESIGNER.Formularios
+{
+    partial class frmBloques
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.gridLunes = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.colN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colHoras = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colInicio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colFin = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colQuitar = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.colEdit = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.gridLunes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridLunes
+            // 
+            this.gridLunes.AllowUserToAddRows = false;
+            this.gridLunes.AllowUserToDeleteRows = false;
+            this.gridLunes.AllowUserToResizeColumns = false;
+            this.gridLunes.AllowUserToResizeRows = false;
+            this.gridLunes.BackgroundColor = System.Drawing.Color.White;
+            this.gridLunes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridLunes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colN,
+            this.colHoras,
+            this.colInicio,
+            this.colFin,
+            this.colDia,
+            this.colQuitar,
+            this.colEdit});
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.ControlDarkDark;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.gridLunes.DefaultCellStyle = dataGridViewCellStyle6;
+            this.gridLunes.Location = new System.Drawing.Point(7, 109);
+            this.gridLunes.MultiSelect = false;
+            this.gridLunes.Name = "gridLunes";
+            this.gridLunes.RowHeadersVisible = false;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.Color.White;
+            this.gridLunes.RowsDefaultCellStyle = dataGridViewCellStyle7;
+            this.gridLunes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridLunes.Size = new System.Drawing.Size(581, 183);
+            this.gridLunes.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(488, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(425, 76);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(57, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(34, 294);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(57, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // colN
+            // 
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colN.DefaultCellStyle = dataGridViewCellStyle1;
+            this.colN.HeaderText = "#";
+            this.colN.Name = "colN";
+            this.colN.Width = 30;
+            // 
+            // colHoras
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colHoras.DefaultCellStyle = dataGridViewCellStyle2;
+            this.colHoras.HeaderText = "Horas";
+            this.colHoras.MaxInputLength = 2;
+            this.colHoras.Name = "colHoras";
+            this.colHoras.Width = 50;
+            // 
+            // colInicio
+            // 
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colInicio.DefaultCellStyle = dataGridViewCellStyle3;
+            this.colInicio.HeaderText = "Inicio";
+            this.colInicio.MaxInputLength = 2;
+            this.colInicio.Name = "colInicio";
+            this.colInicio.Width = 50;
+            // 
+            // colFin
+            // 
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colFin.DefaultCellStyle = dataGridViewCellStyle4;
+            this.colFin.HeaderText = "Fin";
+            this.colFin.Name = "colFin";
+            this.colFin.Width = 50;
+            // 
+            // colDia
+            // 
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colDia.DefaultCellStyle = dataGridViewCellStyle5;
+            this.colDia.HeaderText = "Contenido";
+            this.colDia.Name = "colDia";
+            this.colDia.Width = 280;
+            // 
+            // colQuitar
+            // 
+            this.colQuitar.HeaderText = "Quitar";
+            this.colQuitar.Name = "colQuitar";
+            this.colQuitar.Text = "Quitar";
+            this.colQuitar.Width = 50;
+            // 
+            // colEdit
+            // 
+            this.colEdit.HeaderText = "Editar";
+            this.colEdit.Name = "colEdit";
+            this.colEdit.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.colEdit.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.colEdit.Width = 50;
+            // 
+            // frmBloques
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1291, 766);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.gridLunes);
+            this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmBloques";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmBloques";
+            this.Load += new System.EventHandler(this.frmBloques_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridLunes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridLunes;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colN;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colHoras;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colInicio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colFin;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDia;
+        private System.Windows.Forms.DataGridViewButtonColumn colQuitar;
+        private System.Windows.Forms.DataGridViewButtonColumn colEdit;
+    }
+}
---
+++ DESIGNER/Formularios/frmBloques.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DESIGNER.Formularios
+{
+    public partial class frmBloques : Form
+    {
+        public frmBloques()
+        {
+            InitializeComponent();
+        }
+
+        private void frmBloques_Load(object sender, EventArgs e)
+        {
+            gridLunes.Rows.Add("1", "2", "07:45", "09:15");
+            gridLunes.Rows.Add("2", "2", "07:45", "09:15");
+            gridLunes.Rows.Add("3", "2", "07:45", "09:15");
+            gridLunes.Rows.Add("4", "2", "07:45", "09:15");
+            gridLunes.Rows.Add("5", "2", "07:45", "09:15");
+            gridLunes.Rows.Add("6", "2", "07:45", "09:15");
+            gridLunes.Rows.Add("7", "2", "07:45", "09:15");
+
+            //gridLunes.AlternatingRowsDefaultCellStyle.BackColor = Color.LightCyan;
+            gridLunes.ClearSelection();
+        }
+    }
+}
